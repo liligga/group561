a451b774a52f524469dbb68da68f396040728402
Igor <igglike@gmail.com>
2025-07-22 18:04:38 +0600

    Создал requirements.txt

--- lessons/lesson_1.py
+++ lessons/lesson_1.py
@@ -9,12 +9,12 @@ class Car:
     def drive_to_location(self, location):
        print(f"Driving {self.model} to {location}")
 
-
-subaru_model = "Subaru Forester"
-car_subaru = Car("Forester", 2020)
-car_honda = Car("Honda Fit", 2021)
-print("Model", car_subaru.model, "Year", car_subaru.year, "fined", car_subaru.fined)
-#print(car_honda)
-car_honda.drive_to_location("Kant")
-print(type(subaru_model))
-print(type(car_subaru))
+if __name__ == '__main__':
+    subaru_model = "Subaru Forester"
+    car_subaru = Car("Forester", 2020)
+    car_honda = Car("Honda Fit", 2021)
+    print("Model", car_subaru.model, "Year", car_subaru.year, "fined", car_subaru.fined)
+    #print(car_honda)
+    car_honda.drive_to_location("Kant")
+    print(type(subaru_model))
+    print(type(car_subaru))
